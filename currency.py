85ed6fae06bfdf6b4e4532f707f3daffa9a691e5
access2praveen <pravya@gmail.com>
2021-01-01 10:21:39 +1100

    current coverter

---
+++ currency.py
@@ -0,0 +1,24 @@
+'''
+You’ve probably looked up conversion rates using your browser, but here’s how to automate the task of finding the current exchange rate between any two currencies.
+
+'''
+
+import json
+import sys
+import urllib.request
+
+if len(sys.argv) != 3:
+    print("Usage: ./currencyrates.py lookup_currency base_currency. Example: ./currencyrates.py cad usd")
+    sys.exit()
+
+currency = sys.argv[1]
+basecurrency = sys.argv[2]
+
+currencyurl = "http://freecurrencyrates.com/api/action.php?do=cvals&iso=" + currency + "&f=" + basecurrency + "&v=1&s=cbr"
+f = urllib.request.urlopen(currencyurl)
+obj = json.loads(f.read())
+result = "1 " + currency.upper() + " is "
+result+="{:,.2f}".format(1/obj[currency.upper()]) + " " + basecurrency.upper()
+
+print(result);
+
